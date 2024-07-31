c7fb879041d69f02281615043911d1281938a45a
shokhzod.murodov <s.murodov@uzum.com>
2024-07-31 16:58:26 +0500

    added invite button

---
+++ src/components/button/InviteButton.tsx
@@ -0,0 +1,20 @@
+import {getTelegramId} from "../../infrustructure/telegram/telegram_user";
+import {Button} from "./Button";
+import {IS_LOCAL} from "../../constants/app";
+
+export const InviteButton = () => {
+    const text = "2048 Ton is a fun Telegram mini game where you earn coins by playing, and then use those coins to participate in our airdrop for exciting rewards!\n 💸 +10,000 Coins just for joining as a first-time use";
+
+
+    return <Button width={'w-[15.063rem]'} onClick={() => {
+        const url = IS_LOCAL ?
+            `https://t.me/share/url?text=${text}&url=${encodeURI(`https://t.me/Ton_2048test_bot/start?startapp=ref-${getTelegramId()}`)}&text=${text}` :
+            `https://t.me/share/url?text=${text}&url=${encodeURI(`https://t.me/T2048Bot/start?startapp=ref-${getTelegramId()}`)}`;
+        console.log(url);
+        Telegram.WebApp.openTelegramLink(
+            encodeURI(url)
+        )
+    }}>
+        INVITE A FRIEND
+    </Button>
+}
--- src/infrustructure/telegram/telegram_params.ts
+++ src/infrustructure/telegram/telegram_params.ts
@@ -3,7 +3,7 @@ export function getTelegramParams(): any {
     console.log("getTelegramParams", params)
     return params.split(",")
         .map((e) => {
-            return e.split("=");
+            return e.split("-");
         })
         .filter(e => e.length == 2)
         .reduce((object, arr) => ({...object, [arr[0]]: arr[1]}), {})
